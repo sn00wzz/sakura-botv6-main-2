a5cece04d74327e54af58b0e97e07812f69f7b3e
snowz <isaqueribeiro777@hotmail.com>
2023-03-13 00:42:09 -0300

    novas figurinhas que n sao cringe

---
+++ armor/js/conselhos.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+var palavras = ["*Se você peida muito, logo você tem grandes chances de soltar um tolete de bosta de 30 meters no vaso*"]
+
+module.exports = {
+palavras
+}
